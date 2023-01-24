a9a60ee8d466a75e829b76bdb55bbe61027df329
TheDemo <rsilva4@ucol.mx>
2023-01-24 14:04:56 -0600

    Modulo de consulta funcional

--- consulta.py
+++ consulta.py
@@ -1,28 +1,44 @@
-from main import name, id, price, lot
+import psycopg2
 
-def one(id, name, price, lot):
+DB_NAME = 'postgres'
+DB_USER = 'postgres'
+DB_PASS = 'pass'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+
+def one():
+    conn=psycopg2.connect(dbname=DB_NAME, user=DB_NAME, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+    cur = conn.cursor()
     ok=0
     while ok==0:
         try: #validación de ID
-            id_new = int(input("Ingrese el id del producto: "))
+            new_id = int(input("Ingrese el id de la materia: "))
         except ValueError:
             print("Ingrese un número!")
             ok=+1
             break
-        if id_new not in id:
-            print("El id no existe!")
-            ok=+1            
-            break
+        
+        cur.execute("SELECT * FROM Materia WHERE materiaid="+str(new_id))
+        if cur.rowcount == 0:
+                print("El id no existe!")
         else:
-            print("El producto es: ", name[id.index(id_new)])
-            print("El precio es: ", price[id.index(id_new)])
-            print("La cantidad es: ", lot[id.index(id_new)])
+            cur.execute("SELECT * FROM Materia WHERE materiaid="+str(new_id))
+            res = cur.fetchone()
+            print(f"La materia es: {res[1]}")
+            print(f"la cantidad de temas es: {res[2]}")
+            print(f"el maestro es: {res[3]}")
+            print(f"el grupo es: {res[4]}")
             ok=+1
+            conn.close()
             break
 
-def all(id, name, price, lot):
-    print(f"{'ID':^12}{'Nombre':^12}{'Precio':^12}{'Cantidad':^12}")
-    print("-"*48)
-    for i in range(len(id)):
-        print(f"{id[i]:^10} | {name[i]:^10} | {price[i]:^10} | {lot[i]:^10}") 
-        print("-"*48)
+def all():
+    conn=psycopg2.connect(dbname=DB_NAME, user=DB_NAME, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM Materia")
+    print(f"{'ID':^12}{'Nombre':^12}{'Temas':^12}{'Maestro':^12}{'Grupo':^12}")
+    print("-"*62)
+    for row in cur.fetchall():
+        print(f"{row[0]:^10} | {row[1]:^10} | {row[2]:^10} | {row[3]:^10} | {row[4]:^10}")
+        print("-"*62)
+    conn.close()
